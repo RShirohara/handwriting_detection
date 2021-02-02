60411118224e8548adb5034a581da0992a70d962
Ray Shirohara <47940764+RShirohara@users.noreply.github.com>
2021-02-02 14:44:00 +0900

    refactor: Add thread.daemon option

--- tegaki/send.py
+++ tegaki/send.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 # author: @RShirohara
-# TODO: #4, #7
+# TODO: #7
 
 
 import io
-import threading
+from threading import Event, Thread
 
 from pydub import AudioSegment
 from pydub.playback import play
@@ -23,19 +23,19 @@ def play_audio(source):
     play(audio)
 
 
-class PlayMP3(threading.Thread):
+class PlayMP3(Thread):
     """Play MP3 width multithreading.
 
     Attributes:
         status (Event): Used to indicate if a thread can exec.
-        source (EventQueue): Queue to get source audio.
+        task (EventQueue): Queue to get source audio.
     """
 
-    def __init__(self):
+    def __init__(self, daemon=None):
         """Initialise."""
-        super(PlayMP3, self).__init__()
-        self.status = threading.Event()
-        self.source = EventQueue(self.status)
+        super(PlayMP3, self).__init__(daemon=daemon)
+        self.status = Event()
+        self.task = EventQueue(self.status)
 
     def run(self):
         """Run thread."""
@@ -43,5 +43,5 @@ class PlayMP3(threading.Thread):
         while True:
             if not self.status.is_set():
                 self.status.wait()
-            src = self.source.w_get()
+            src = self.task.w_get()
             play_audio(src)
