ff42511a5cf61fd4072323d4d01cd84e2b74146b
Ray Shirohara <47940764+RShirohara@users.noreply.github.com>
2021-02-04 17:01:52 +0900

    feat: add detection class
    Add class "DetectArea" in detect.
    
    close #1, #2

--- tegaki/detect.py
+++ tegaki/detect.py
@@ -3,14 +3,26 @@
 # TODO: #1, #2, #3
 
 
-from multiprocessing import Process
-from threading import Event
+from threading import Event, Thread
+from typing import NamedTuple
 
 import cv2
 import numpy as np
 import tensorflow as tf
 
 from .util import EventQueue
+
+
+class DetectedGrid(NamedTuple):
+    """Detected object coordinate."""
+
+    xmin: int
+    xmax: int
+    ymin: int
+    ymax: int
+
+    def get(self):
+        return (self.xmin, self.xmax), (self.ymin, self.ymax)
 
 
 def load_inference_graph(root):
@@ -20,8 +32,8 @@ def load_inference_graph(root):
         root (str): Path to the directory where the tensorflow model exists.
 
     Returns:
-        detection_graph (Graph): tf.Graph object.
-        sess (Session): tf.Session object.
+        Graph: tf.Graph object.
+        Session: tf.Session object.
     """
 
     ckpt_path = root + '/frozen_inference_graph.pb'
@@ -46,7 +58,7 @@ def detect_hands(image_np, detection_graph, sess, thresh=0.7):
         sess (Session): tf.Session object.
 
     Returns:
-        result (tuple[ndarray]): Detected boxes.
+        tuple[ndarray]: Detected boxes.
     """
 
     image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
@@ -92,7 +104,7 @@ def detect_paper(image_cv, thresh_level=(140, 255), max_area=100000):
         thresh (tuple[int]): Threshold of the grayscale. (max, min)
         max_area (int): Max area of the paper.
     Returns:
-        boxes (tuple): Detected boxes.
+        tuple[ndarray]: Detected boxes.
     """
 
     image_gray = cv2.cvtColor(image_cv, cv2.COLOR_BGR2GRAY)
@@ -110,3 +122,85 @@ def detect_paper(image_cv, thresh_level=(140, 255), max_area=100000):
     ])
 
     return boxes
+
+
+class DetectArea(Thread):
+    """Detect paper and hands with multiprocessing.
+
+    Attributes.
+        status (Event): Used to indicate if a thread can exec.
+        task (EventQueue[ndarray]): Queue to get source image converted to BGR.
+        result (EventQueue[ndarray]): Qurue pointer to send results.
+    """
+
+    def __init__(self, result, model_dir, dev_info, daemon=None):
+        """Initialize.
+
+        Args:
+            result (EventQueue[ndarray]): Queue pointer to send results.
+            model_dir (str): Path to directory where tensorflow model exists.
+            dev_info (CapParams): Capture device infomation.
+        """
+
+        super(DetectArea, self).__init__(daemon=daemon)
+        self.status = Event()
+        self.task = EventQueue(self.status)
+        self.result = result
+        self.graph, self.sess = load_inference_graph(model_dir)
+        self.dev_info = dev_info
+
+    def run(self):
+        """Exec."""
+
+        while True:
+            if not self.status.is_set():
+                self.status.wait()
+            source = self.task.w_get()
+
+            paper = detect_paper(source)
+            if not paper:
+                continue
+
+            hands = detect_hands(source, self.graph, self.sess)
+            if not hands:
+                continue
+
+            paper_grid = tuple([
+                DetectedGrid(
+                    min(p[::2]),
+                    max(p[::2]),
+                    min(p[1::2]),
+                    max(p[1::2])
+                )
+                for p in paper
+            ])
+            hands_grid = tuple([
+                DetectedGrid(
+                    int(h[1] * self.dev_info.width),
+                    int(h[3] * self.dev_info.width),
+                    int(h[0] * self.dev_info.height),
+                    int(h[2] * self.dev_info.height)
+                )
+                for h in hands
+            ])
+
+            _result = []
+            for pa in paper_grid:
+                for ha in hands_grid:
+                    _res = []
+                    for p, h in zip(pa.get(), ha.get()):
+                        if not (
+                            p[0] < h[0] < h[1] < p[1] and
+                            h[0] < p[0] < p[1] < h[1] and
+                            p[0] < h[0] < p[1] < h[1] and
+                            h[0] < p[0] < h[1] < p[1]
+                        ):
+                            _res.append(True)
+                        else:
+                            _res.append(False)
+                    _result.append(tuple(_res))
+
+            if (True, True) in _result:
+                self.result.w_put(source)
+
+        self.sess.close()
